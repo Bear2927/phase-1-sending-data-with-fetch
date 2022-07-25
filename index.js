1ff9a75837d75b0c4c0e25dff231c3ac15a863e0
Barrett Hiltabrand <bear2927@yahoo.com>
2022-07-25 15:47:05 -0500

    commit tree 50591c258b8379508aea70160f8ddc7115e0f34c

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+
+function submitData(name, email) {
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(resp => resp.json())
+    .then(console.log)
+    .catch(error => console.log(error.message))
+}
+
+submitData('Steve', 'steve@steve.com')
